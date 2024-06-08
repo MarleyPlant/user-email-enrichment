963cd78fdaf11ef4e903cf019e9e2bcf15f26912
Marley Plant <marley@marleyplant.com>
2024-06-08 07:20:31 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import emailToName from 'email-to-name';
 import { getCompanyFromEmail, getGitHubInfo, getGitlab, getGravatar } from './providers/index.js';
 import { transform } from './transform.js';
+const enrich = async (email) => {
+  if (!email) {
+    throw "error";
+  } else {
+    let results = {};
+    let providers = {"name": emailToName.process, "company": getCompanyFromEmail, "github": getGitHubInfo, "gravatar": getGravatar};
+    for (let [key, value] of Object.entries(providers)) {
+      if (value == undefined) {
+        throw "error";
+      }
 
-const enrich = async email => {
-    if (!email) {
-        throw 'error';
-    } else {
-        const nameFromEmail = emailToName.process(email);
-        const companyFromEmail = getCompanyFromEmail(email);
-        const ghProfile = await getGitHubInfo(email);
-        const glProfile = await getGitlab(email);
-        const gravatar = await getGravatar(email);
-        return transform(ghProfile, glProfile, gravatar, nameFromEmail, companyFromEmail);
+      try {
+        results[key] = await value(email);
+      } catch (e) {
+        console.error(e);
+      }
+
     }
-};
+    return results;
 
+    // return transform(ghProfile, gravatar, nameFromEmail, companyFromEmail);
+  }
+};
 export default enrich;
